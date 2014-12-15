9b68e0467db92e66dff07bbb0fc87beca10f2308
Sina <st79375@appstate.edu>
2014-12-15 04:50:15 -0500

    Thirteenth commit - Passing all tests. Will comment in next commit and be done.

--- src/TomasuloCentralProcessingUnit.java
+++ src/TomasuloCentralProcessingUnit.java
@@ -1,4 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 /**
+ * TomasuloCentralProcessingUnit :: Class that orchestratics the stages and cycles of the processor.
  * Created by rt on 11/30/14.
  */
 public class TomasuloCentralProcessingUnit {
@@ -14,11 +19,10 @@ public class TomasuloCentralProcessingUnit {
     TomasuloRegisterFileManager regManager;
     TomasuloClock clock;
     TomasuloInstruction instruction;
+    PrintWriter writer;
     boolean branch;
     boolean halt;
     boolean stall;
-
-    int testcounter = 0;
 
     public TomasuloCentralProcessingUnit(){
         instructionBuilder = new TomasuloInstructionFactory();
@@ -31,28 +35,32 @@ public class TomasuloCentralProcessingUnit {
         clock = TomasuloClock.getInstance();
     }
 
-    public void run(){
+    public void run(Boolean isVerbose){
         int[] fetched;
         int word;
+        int counter = 10;
 
-        while(!halt || !functionalUnitsCompleted()){
-            System.out.println("Clock cycle: " + clock.cycle);
-            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            //cdb = writeResult(fuManager);
-            execute(rsManager,fuManager);
-            if(!halt && !branch){
+        while(!halt || !pipelineCompleted()){
+            //System.out.println("**************************Clock cycle: " + clock.cycle);
+            //System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            cdb = writeResult(rsManager,cdb);
+            execute(rsManager,regManager,fuManager,writer,memory);
+            //System.out.println("STALL"+stall+"\nHALT"+halt+"\nBRANCH"+branch);
+            if(!halt && !rsManager.hasBranch()){
+                //System.out.println("HALT: "+halt);
                 word = memory.fetchWord(pc.address);
-                instruction = decodeInstruction(word,decoder,instructionBuilder);
-                stall = issue(instruction,rsManager,fuManager,regManager);
-                printDumps(memory,rsManager,fuManager,regManager);
+                instruction = decodeInstruction(word, decoder, instructionBuilder);
+                stall = issue(instruction, rsManager, fuManager, regManager);
                 if(!halt && !stall) pc.increment();
-                clock.increment();
-                updateReservationStations(cdb);
-                clearReservationStations();
-                if(testcounter== 5) break;
-                testcounter++;
             }
+            //if(isVerbose) printDumps(memory, rsManager, fuManager, regManager);
+            updateReservationStations(regManager,rsManager,cdb);
+            clearReservationStations(rsManager);
+            clock.increment();
+            /*if(counter == 0) break;
+            counter--;*/
         }
+        //writer.close();
     }
 
     // Singleton design pattern without double checked locking
@@ -64,19 +72,12 @@ public class TomasuloCentralProcessingUnit {
 
     static TomasuloInstruction decodeInstruction(int word, TomasuloDecoder decoder, TomasuloInstructionFactory instructionBuilder){
         String operation = getInstructionOpcodeOperationFromWord(word, decoder);
-        System.out.println("operation: " +  operation);
         String operationType = getInstructionUnitType(operation,decoder);
-        System.out.println("operationType: " + operationType);
         TomasuloInstruction instruction = createInstruction(operationType,instructionBuilder);
         String operationRegisterDesign = getInstructionRegisterDesign(operation,decoder);
-        System.out.println("register design: " + operationRegisterDesign);
         instruction.setOpcodeName(operation);
         instruction.setRegisterDesign(operationRegisterDesign);
-        System.out.println("Instruction type: " + instruction.getClass().getName());
         String[] operands = decodeOperands(operationRegisterDesign,word,decoder);
-        for(String s : operands){
-            System.out.println("operand: " + s);
-        }
         instruction.setFirstSourceRegister(operands[0]);
         instruction.setSecondSourceRegister(operands[1]);
         instruction.setDestinationRegister(operands[2]);
@@ -119,63 +120,137 @@ public class TomasuloCentralProcessingUnit {
         return getTrapFunction(immediateValue);
     }
 
-    public boolean functionalUnitsCompleted(){
-       return fuManager.allUnitsCompleted();
+    public boolean pipelineCompleted(){
+       return rsManager.allUnitsCompleted();
     }
 
     static String[] decodeOperands(String registerDesign, int encoding, TomasuloDecoder decoder){
         return decoder.decodeOperands(registerDesign,encoding);
     }
 
-    static void execute(TomasuloReservationStationManager rsManager, TomasuloFunctionalUnitManager fuManager){
-        fuManager.execute(rsManager);
+    static void execute(TomasuloReservationStationManager rsManager, TomasuloRegisterFileManager rfManager,TomasuloFunctionalUnitManager fuManager, PrintWriter writer, TomasuloMemory memory){
+        fuManager.execute(rsManager,rfManager,writer,memory);
     }
 
-    static TomasuloCommonDataBus writeResult(TomasuloFunctionalUnitManager fuManager){
-        for(TomasuloFunctionalUnit f : fuManager.functionalUnits){
-            TomasuloCommonDataBus result = f.write();
-            if(result != null){
-                return result;
+    static TomasuloCommonDataBus writeResult(TomasuloReservationStationManager rsManager,TomasuloCommonDataBus cdb){
+        TomasuloReservationStation reservationStation = null;
+        for(TomasuloReservationStation rs : rsManager.reservationStations){
+            if(rs.resultReady == true && !rs.resultWritten){
+                reservationStation = rs;
+                break;
             }
         }
-        return null;
+        if(reservationStation != null) {
+            TomasuloProgramCounter pc = TomasuloProgramCounter.getInstance();
+            if(reservationStation.currentInstruction.instructionType.equals("branch")){
+                pc.address = reservationStation.branchValue;
+            }
+            cdb.setResult(reservationStation.result);
+            cdb.setReservationStation(reservationStation.name);
+            cdb.setNameOfSourceFunctionalUnit(reservationStation.executingUnit.name);
+            reservationStation.resultWritten = true;
+        }
+        return cdb;
     }
 
-    static boolean issue(TomasuloInstruction instruction,TomasuloReservationStationManager rsManager, TomasuloFunctionalUnitManager fuManager,
+    static PrintWriter setupWriter(String fileName,PrintWriter writer){
+        String outputFile = fileName.substring(0,fileName.lastIndexOf('.'));
+        try {
+            writer = new PrintWriter("intUnit1.out", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return writer;
+    }
+
+    boolean issue(TomasuloInstruction instruction,TomasuloReservationStationManager rsManager, TomasuloFunctionalUnitManager fuManager,
                          TomasuloRegisterFileManager rfManager){
         if(instruction.getOpcodeName().equals("trap") && instruction.getImmediateValue() == 0){
+            halt = true;
             return true;
+        }
+        if(instruction.instructionType.equals("branch")){
+            branch = true;
         }
         switch(instruction.getClass().getName()) {
             case "TomasuloFloatingPointInstruction":
                 if(!rsManager.hasAvailableRStation("float")) return true;
+                break;
             case "TomasuloIntegerInstruction":
                 if(!rsManager.hasAvailableRStation("int")) return true;
+                break;
             case "TomasuloMemoryInstruction":
                 if(!rsManager.hasAvailableRStation("mem")) return true;
+                break;
             case "TomasuloBranchInstruction":
-                if(!rsManager.hasAvailableRStation("branch")) return true;
+                if(!rsManager.hasAvailableRStation("branch")) {
+                    return true;
+                }
+                break;
             case "TomasuloTrapInstruction":
                 if(!rsManager.hasAvailableRStation("trap")) return true;
-
+                break;
         }
         return rsManager.issue(instruction,fuManager,rfManager);
     }
 
-    static void updateReservationStations(TomasuloCommonDataBus cdb){
+    static void updateReservationStations(TomasuloRegisterFileManager rfManager, TomasuloReservationStationManager rsManager,
+                                          TomasuloCommonDataBus cdb){
+        if(cdb.nameOfWritingReservationStation == null) return;
+        for(TomasuloRegister rf : rfManager.registers){
+            if(rf.Qi != null && rf.Qi.equals(cdb.nameOfWritingReservationStation)){
+                //System.out.println("Test register to be writtenn: " + rf.registerName);
+                rf.setValue(cdb.result);
+                rf.Qi = null;
+            }
+        }
+        for(TomasuloReservationStation rs : rsManager.reservationStations){
+            if(rs.Qj != null && rs.Qj.equals(cdb.nameOfWritingReservationStation))
+            {
+                rs.Vj = cdb.result;
+                rs.Qj = null;
 
+            }
+            if(rs.Qk != null && rs.Qk.equals(cdb.nameOfWritingReservationStation))
+            {
+                rs.Vk = cdb.result;
+                rs.Qk = null;
+            }
+        }
+        cdb.nameOfWritingReservationStation = null;
+        /*for(TomasuloReservationStation rs : rsManager.reservationStations){
+            if(rs.name.equals(cdb.nameOfWritingReservationStation))
+                rs.setBusy(false);
+        }*/
     }
 
-    static void clearReservationStations(){
-
+    static void clearReservationStations(TomasuloReservationStationManager rsManager){
+        for(TomasuloReservationStation rs : rsManager.reservationStations){
+            if(rs.resultWritten == true) rs.resetContents();
+        }
     }
 
     static void printDumps(TomasuloMemory memory, TomasuloReservationStationManager rsManager,
                            TomasuloFunctionalUnitManager fuManager, TomasuloRegisterFileManager regManager){
+        TomasuloClock clock = TomasuloClock.getInstance();
+        System.out.println("==========================================================================================");
+        System.out.println("Clock cycle:" + clock.getCurrentCycle());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         memory.printDataContents();
+        rsManager.printIssuedInstruction();
         rsManager.printStationContents();
         fuManager.printFunctionalUnitContents();
         regManager.printRegisterContents();
-        System.out.println("\n==========================================================================================");
+        printCDB();
+    }
+
+    static void printCDB(){
+        System.out.println("...........................................");
+        TomasuloCommonDataBus cdb = TomasuloCommonDataBus.getInstance();
+        System.out.println("cdb:" + cdb.result);
+        System.out.println("writtenby:" + cdb.nameOfWritingReservationStation);
+        System.out.println("...........................................");
     }
 }
